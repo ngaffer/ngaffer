2cf739b84691c3f2b5a3e62e8c3e2b8e90232a08
Natasha Gaffer <ngaffer@gmail.com>
2018-04-29 22:56:49 -0700

    Setup SASS and a single server route

---
+++ server/routes/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+// Connect
+mongoose.connect('mongodb://localhost:27017/ngaffer');
+
+router.get('/test', function(req, res, next) {
+  res.send('hello');
+});
+
+router.post('/test', function(req, ress, next) {
+  let email = req.body.email;
+  let user = new User({
+    firstName: 'Natasha',
+    lastName: 'Gaffer',
+    password: 'testpass',
+    email: email
+  });
+  user.save();
+  res.redirect('/');
+});
+
+module.exports = router;
